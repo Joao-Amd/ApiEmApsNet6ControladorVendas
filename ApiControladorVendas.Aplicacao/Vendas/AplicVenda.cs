1bd784d1117806f8657d59e9eddc3bc2096c11a5
Joao-Amd <joao07medeiros@gmail.com>
2024-03-10 15:59:22 -0300

    Realizado criação da aplicação de Vendas

---
+++ ApiControladorVendas.Aplicacao/Vendas/AplicVenda.cs
@@ -0,0 +1,33 @@
+﻿using ApiControladorVendas.Aplicacao.Vendas.Dtos;
+using ApiControladorVendas.Aplicacao.Vendas.Views;
+
+namespace ApiControladorVendas.Aplicacao.Vendas
+{
+    public class AplicVend : IAplicVenda
+    {
+        public VendaViewModel Alterar(int id, VendaDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inserir(VendaDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VendaViewModel Recuperar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<VendaViewModel> RecuperarVenda()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ApiControladorVendas.Aplicacao/Vendas/IAplicVenda.cs
@@ -0,0 +1,14 @@
+﻿using ApiControladorVendas.Aplicacao.Vendas.Dtos;
+using ApiControladorVendas.Aplicacao.Vendas.Views;
+
+namespace ApiControladorVendas.Aplicacao.Vendas
+{
+    public interface IAplicVenda
+    {
+        VendaViewModel Recuperar(int id);
+        List<VendaViewModel> RecuperarVenda();
+        void Inserir(VendaDto dto);
+        void Deletar(int id);
+        VendaViewModel Alterar(int id, VendaDto dto);
+    }
+}
