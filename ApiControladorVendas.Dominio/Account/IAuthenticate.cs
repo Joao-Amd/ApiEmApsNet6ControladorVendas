bdc6f67c9ca6cde5f25a0233a0d9728074b8046c
João Vitor de Almeida Medeiros <joao.medeiros@useall365.onmicrosoft.com>
2024-04-09 12:58:18 -0300

    Criado parte de authenticate

---
+++ ApiControladorVendas.Dominio/Account/IAuthenticate.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiControladorVendas.Dominio.Account
+{
+    public interface IAuthenticate
+    {
+        bool AuthenticateAsync(string email, string senha);
+        bool UserExists(string email);
+        public string GenerateToken(int id, string email);
+
+    }
+}
