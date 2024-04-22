a3fc5b288c0de9d9eb866ff0afd197f13dcc2bb9
João Vitor de Almeida Medeiros <joao.medeiros@useall365.onmicrosoft.com>
2024-04-22 13:42:30 -0300

    Iniciado a criação de testes Unitarios

---
+++ ApiControladorVendaUnitTests/DbUnitTestsMockInitializer.cs
@@ -0,0 +1,17 @@
+﻿
+using ApiControladorVendas.Dominio.Usuarios;
+using ApiControladorVendas.Repositorio.Contextos;
+
+namespace ApiControladorVendaUnitTests
+{
+    public class DbUnitTestsMockInitializer
+    {
+        public DbUnitTestsMockInitializer(){}
+
+        public void Seed(Contexto contexto)
+        {
+            contexto.Usuarios.Add
+                (Usuario.Novo("teste", "teste@gmail.com"));
+        }
+    }
+}
