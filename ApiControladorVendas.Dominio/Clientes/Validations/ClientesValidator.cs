9a9432f6735ca726c3099485a4a0c0da9cc8a37c
Joao-Amd <joao07medeiros@gmail.com>
2024-05-13 08:35:12 -0300

    Adcionado validation do usuario

--- ApiControladorVendas.Dominio/Clientes/Validations/ClientesValidator.cs
+++ ApiControladorVendas.Dominio/Clientes/Validations/ClientesValidator.cs
@@ -7,20 +7,34 @@ namespace ApiControladorVendas.Dominio.Clientes.Validations
     {
         public ClientesValidator()
         {
-            
+            _nome();
+            _cpf();
+            _celular();
+            _cep();
+            _email();
+            _endereco();
+            _numero();
+            _complemento();
+            _bairro();
+            _cidade();
+            _estado();
         }
 
         private  void _nome()
         {
-
+            RuleFor(x => x.Nome)
+               .NotEmpty().WithMessage("Nome deve ser obrigatório")
+               .Matches("^[A-Za-zÀ-ÖØ-öø-ÿ\\s]+$").WithMessage("Nome deve conter apenas letras e espaços");
         }
         private void _cpf()
         {
-
+            RuleFor(x => x.Cpf)
+                .NotEmpty().WithMessage("CPF deve  ser obrigatório")
+                .IsValidCPF().WithMessage("CPF inválido");
         }
         private void _celular()
         {
-
+            RuleFor(x => x.Celular)
         }
         private void _cep()
         {
